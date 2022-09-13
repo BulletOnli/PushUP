ab00d36f035b778978910e790e58014b1705058a
Gemmuel <gemmueldelacruz@gmail.com>
2022-09-13 20:28:49 +0800

    fixed rep counts

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const count = document.getElementById("count")
 reps = 0
 
 repBtn.addEventListener("click", function() {
+    reps -= 1
     goUp()
     goDown()
     pushUp = setInterval(goUp, 1000)
@@ -15,18 +16,17 @@ repBtn.addEventListener("click", function() {
 
 stopBtn.addEventListener("click", function() {
     reps = 0 
-    count.textContent = `Count: 0`
     clearInterval(pushUp)
     clearInterval(pushDown)
 })
 
 function goUp() {
     img.style.backgroundImage = "url('up.png')";
-
-    reps++
-    count.textContent = `Count: ${reps}`
 }
 
 function goDown() {
     img.style.backgroundImage = "url('down.png')";
+
+    reps++
+    count.textContent = `Count: ${reps}`
 }
